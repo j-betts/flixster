4e40d17a084fb241d2923d85c7f80e07f6f2a9df
Justin Betts <bett9421@m506-118-26437.local>
2020-02-01 19:44:58 -0800

    movie titles work

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -8,12 +8,71 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        print("MoviesViewController loaded. Joy!")
+        
+        // have to add these two lines for some reason. Don't really understand why, just have to do it.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // big block of code that we copied in to handle requesting of data through api
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                // after movies have been populated, now we need to call those two functions again. First time they were called there were no movies yet.
+                self.tableView.reloadData()
+                
+                print(dataDictionary)
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+            }
+        }
+        task.resume()
+    }
+    
+    // how many cells do we want to create? one for each movie.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    // called for each movie
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // this was the basic one that was boring and only had some text
+        //let cell = UITableViewCell()
+        
+        // this is the new version with poster, title, and synopsis
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
+        // get data from that json object from the api
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        
+        //cell.textLabel!.text = title as! String
+        cell.titleLable.text = title
+        
+        return cell
     }
     
 
