cfcde7e5c468cef262b276eec3fa971adc86bdf7
Justin Betts <bett9421@m506-118-26437.local>
2020-02-03 17:52:23 -0800

    working on movie details view, at 5:00 min of video

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Justin Betts on 2/3/20.
+//  Copyright © 2020 Justin Betts. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: [String:Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print(movie["title"])
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        //cell.posterView.af_setImage(withURL: posterUrl!)
+        
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["overview"] as? String
+
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
