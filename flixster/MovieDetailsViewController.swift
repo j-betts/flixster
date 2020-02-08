be9db5a82815ae77b328ddc7530c714d80280d16
Justin Betts <bett9421@m506-118-26437.local>
2020-02-08 15:24:38 -0800

    details view is working

--- flixster/MovieDetailsViewController.swift
+++ flixster/MovieDetailsViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import AlamofireImage
 class MovieDetailsViewController: UIViewController {
 
     
@@ -28,10 +28,17 @@ class MovieDetailsViewController: UIViewController {
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
-        //cell.posterView.af_setImage(withURL: posterUrl!)
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropUrl!)
         
         titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
         synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
 
 
     }
